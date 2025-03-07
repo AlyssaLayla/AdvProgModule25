a478c6f854d9e19be4f4f5004ff2bb05105ca7fd
AlyssaLayla <alyssasasti@gmail.com>
2025-03-07 20:05:31 +0700

    [GREEN] Implement PaymentVoucherCode class

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/PaymentVoucherCode.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/PaymentVoucherCode.java
@@ -3,11 +3,11 @@ package id.ac.ui.cs.advprog.eshop.model;
 import java.util.Map;
 
 public class PaymentVoucherCode extends Payment {
-    public PaymentVoucherCode(String id, String method, Map<String, String> paymentData) {
-        super(id, method, paymentData);
+    public PaymentVoucherCode(String id, Order order, Map<String, String> paymentData) {
+        super(id, "VoucherCode", order, paymentData);
     }
 
-    public PaymentVoucherCode(String id, String method, Map<String, String> paymentData, String status) {
-        super(id, method, paymentData, status);
+    public PaymentVoucherCode(String id, Order order, Map<String, String> paymentData, String status) {
+        super(id, "VoucherCode", order, paymentData, status);
     }
 }
